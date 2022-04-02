36cecf10d72b3d6a3aa28d06797f208534bb0ccf
Steve Hannah <steve@weblite.ca>
2022-04-02 08:07:17 -0700

    Removed install.sql

--- install.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `example-app`;
-USE `example-app`;
-
-            
-        CREATE TABLE IF NOT EXISTS `test` (
-            test_id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-            test_field VARCHAR(100) 
-        );
